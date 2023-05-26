f7f36e65ba5e8889f3cf01100677dd93994842c9
Anett98 <anett.mk.98@gmail.com>
2023-05-27 02:14:30 +0400

    improving logic and deleting unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,12 @@ export default function App() {
 
   const handleSearchPhoto = async (e) => {
     e.preventDefault();
-    const splitedKeywords = keywords.split(" ");
-    setKeywordsArray(splitedKeywords);
+    const splitedKeywords = keywords.trim().split(" ");
+    setKeywordsArray(splitedKeywords.filter((word,index) => splitedKeywords.indexOf(word) === index));
     const updatedSearchResults = [];
     Promise.all(
       splitedKeywords?.map((keyword) =>
-        fetch(
-          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f620c0943c6d0dd798a333e25cf497a8&tags=${keyword}&license=0&per_page=5&page=1&format=json&nojsoncallback=1`
-        )
+        fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f620c0943c6d0dd798a333e25cf497a8&tags=${keyword}&license=0&per_page=5&page=1&format=json&nojsoncallback=1`)
       )
     )
       .then((results) => Promise.all(results.map((result) => result.json())))
@@ -32,7 +30,7 @@ export default function App() {
             .slice(0, 5)
             .map((photo) => {
               return {
-                id: photo.id,
+                id: photo.id + Math.random(),
                 imageTitle: photo.title,
                 keywordName: splitedKeywords[currentKeywordIndex],
                 imageUrl: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
